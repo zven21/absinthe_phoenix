4c83e1d633aa068f0f90f4ff2e65a95377b0619c
linchuan <linchuan@helijia.com>
2018-10-24 12:06:35 +0800

    support *-dev *-rc *-beta.

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule Absinthe.Phoenix.Mixfile do
       licenses: ["MIT"],
       links: %{
         "Website": "https://absinthe-graphql.org",
-        "Changelog": "https://github.com/absinthe-graphql/absinthe_phoenix/blob/master/CHANGELOG.md", 
+        "Changelog": "https://github.com/absinthe-graphql/absinthe_phoenix/blob/master/CHANGELOG.md",
         "GitHub": "https://github.com/absinthe-graphql/absinthe_phoenix"
       }
     ]
@@ -43,7 +43,7 @@ defmodule Absinthe.Phoenix.Mixfile do
       {:absinthe_plug, "~> 1.4.0"},
       {:absinthe, "~> 1.4.0"},
       {:decimal, "~> 1.0"},
-      {:phoenix, "~> 1.2"},
+      {:phoenix, "~> 1.2", allow_pre: true},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_html, "~> 2.10.5 or ~> 2.11", optional: true},
       {:ex_doc, "~> 0.14", only: :dev},
